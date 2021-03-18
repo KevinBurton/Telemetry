e7d645f912c1f9f4293cf8efa2fab8f57b32d496
Ronald Kevin Burton <rkevinburton@charter.net>
2021-03-17 23:08:16 -0500

    Added functioning log to Simulator

--- Simulator/Controls/Log/LogItemViewModel.cs
+++ Simulator/Controls/Log/LogItemViewModel.cs
@@ -13,6 +13,38 @@ namespace Simulator.Controls
             Message = message;
         }
 
-        public string Message { get; }
+        private string _localMessage;
+        public string Message
+        {
+            get => _localMessage;
+            set
+            {
+                if (_localMessage != value)
+                {
+                    _localMessage = value;
+                    RaisePropertyChanged();
+                    RaisePropertyChanged("MessageBitString");
+                }
+            }
+        }
+        public string MessageBitString
+        {
+            get
+            {
+                var pad = new byte[4] { 0, 0, 0, 0 };
+                if (Message != null)
+                {
+                    var values = Encoding.ASCII.GetBytes(Message);
+                    for (int i = 0; i < values.Length && i < pad.Length; i++)
+                    {
+                        pad[i] = values[i];
+                    }
+                }
+                return Convert.ToString((byte)(pad[0] & 0xFFFF), 2).PadLeft(8, '0') +
+                       Convert.ToString((byte)(pad[1] & 0xFFFF), 2).PadLeft(8, '0') +
+                       Convert.ToString((byte)(pad[2] & 0xFFFF), 2).PadLeft(8, '0') +
+                       Convert.ToString((byte)(pad[3] & 0xFFFF), 2).PadLeft(8, '0');
+            }
+        }
     }
 }
--- Simulator/Controls/Log/LogViewModel.cs
+++ Simulator/Controls/Log/LogViewModel.cs
@@ -1,10 +1,4 @@
 ﻿using Simulator.Common.Models;
-using Simulator.ViewModel.Command;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Simulator.Controls
 {
